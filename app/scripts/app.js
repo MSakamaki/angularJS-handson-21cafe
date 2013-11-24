23fc9734299444013a09a3558e217a07a6bf9324
MSakamaki <daigouzigai5@gmail.com>
2013-11-25 02:15:12 +0900

    issues/2-0 1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,6 +15,7 @@ angular.module('angularJSAppApp', [
 	.config(function ($routeProvider) {
 		$routeProvider
             .when('/main', { templateUrl: 'views/main.html', controller: 'MainCtrl' })
+            .when('/main2', { templateUrl: 'views/main2.html', controller: 'MainCtrl2' })
             .when('/show', {templateUrl:'views/show.html', controller : 'ShowCtrl'})
             .otherwise({ redirectTo: '/main' });
 	});
