f5b03f7eee2771ad1a24bf5daaa6cc25f656c633
MSakamaki <daigouzigai5@gmail.com>
2013-11-24 23:48:51 +0900

    issues/1-0 2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,19 +4,10 @@ angular.module('angularJSAppApp', [
 	'ngCookies',
 	'ngRoute',
 	'angularLocalStorage', 
-	'ngGrid',
-	'btns', 
-	'formApp', 
-	'todoTags', 
-	'utill', 
-	'myitems',
-	'myvalidate',
-	'myconst'
+	'ngGrid'
 	])
 	.config(function ($routeProvider) {
 		$routeProvider
             .when('/main', { templateUrl: 'views/main.html', controller: 'MainCtrl' })
-            .when('/main2', { templateUrl: 'views/main2.html', controller: 'MainCtrl2' })
-            .when('/show', {templateUrl:'views/show.html', controller : 'ShowCtrl'})
             .otherwise({ redirectTo: '/main' });
 	});
