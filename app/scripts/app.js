4e0c86025abc745968c802bdbc5f3c43a49f5dd8
MSakamaki <daigouzigai5@gmail.com>
2013-11-25 20:36:22 +0900

    issues/1-0

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,21 +2,12 @@
 
 angular.module('angularJSAppApp', [
 	'ngCookies',
-	'ngRoute',  // <-Angular-routeを読み込み
+	'ngRoute',
 	'angularLocalStorage', 
-	'ngGrid',
-	'btns', 
-	'formApp', 
-	'todoTags', 
-	'utill', 
-	'myitems',
-	'myvalidate',
-	'myconst'
+	'ngGrid'
 	])
 	.config(function ($routeProvider) {
 		$routeProvider
             .when('/main', { templateUrl: 'views/main.html', controller: 'MainCtrl' })
-            .when('/main2', { templateUrl: 'views/main2.html', controller: 'MainCtrl2' })
-            .when('/show', {templateUrl:'views/show.html', controller : 'ShowCtrl'})
             .otherwise({ redirectTo: '/main' });
 	});
--- app/scripts/service/constant.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('myconst', [])
-    .constant('MESSAGES', {
-       TITLE_LENGTH_ERR : "タイトルは5文字以上10文字以下で入力してください。" ,
-       TITLE_OVERLAP_ERR : "タイトルが重複しています。" ,
-       TITLE_REQUIRED : "タイトルは必須入力です。",
-       TEXT_LENGTH_ERR : "テキストは140文字までで入力してください。"
-    });
