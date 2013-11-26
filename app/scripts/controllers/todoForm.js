5c5999ab03fde4ececb4e9c900b56b0aa92539f4
MSakamaki <daigouzigai5@gmail.com>
2013-11-27 01:36:48 +0900

    issues/2-1

--- app/scripts/controllers/todoForm.js
+++ app/scripts/controllers/todoForm.js
@@ -1,27 +1,9 @@
 'use strict';
 
 angular.module('formApp', [])
-	.controller('todoForm', function ($scope, MESSAGES) {
-
-		$scope.msgTitleRequired = MESSAGES.TITLE_REQUIRED;
-		$scope.msgTitleLengthError = MESSAGES.TITLE_LENGTH_ERR;
-		$scope.msgTextLengthError = MESSAGES.TEXT_LENGTH_ERR;
+	.controller('todoForm', function ($scope) {
 
 		$scope.insert=function(){
-			if ($scope.isFormError()) return;
 			$scope.items.push({ title : $scope.inTitle, text : $scope.inText });
-		};
-
-		$scope.isFormError = function(){
-			return $scope.isTitleError() || $scope.isTextError();
-		};
-
-		$scope.isTitleError = function(){
-			return $scope.myForm.title.$error.required ||
-				$scope.myForm.title.$error.maxlength ||
-				$scope.myForm.title.$error.minlength ;
-		};
-		$scope.isTextError = function(){
-			return $scope.myForm.text.$error.maxlength;
 		};
     });
