efe6af7bd4f4cfdc57114b5d9fd7c69f3a8a7e5a
MSakamaki <daigouzigai5@gmail.com>
2013-11-29 23:55:49 +0900

    ng-repeat track by key

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,10 +4,4 @@ angular.module('angularJSAppApp')
         var defAry=[];
         storage.bind($scope,'items', {defaultValue: defAry ,storeName: 'todoAPP'});
 
-        $scope.$watch('items',function(){
-			$scope.lists=[];
-			$scope.items.some(function(v){
-				$scope.lists.push({text: v.text ,title: v.title});
-			});
-		}, true);
     });
